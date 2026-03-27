b5ebae29c648e03265e2df67c53d4e683b160d22
Bambang-herliandy <andytmvn@gmail.com>
2026-03-27 07:55:17 +0700

    fix:layout from frontend side

--- final-project/src/app/layout.tsx
+++ final-project/src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import LogoutButton from "@/components/LogoutButton";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jakarta = Plus_Jakarta_Sans({
+  variable: "--font-jakarta",
   subsets: ["latin"],
 });
 
@@ -26,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${jakarta.variable} font-sans antialiased`}>
         {/* <Link href={'/'}>Home</Link>
         <Link href={'/login'}>Login</Link>
         <Link href={'/register'}>Register</Link>
