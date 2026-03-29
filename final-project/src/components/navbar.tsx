be7c276cb8b4daaa291927d6e179968528ffc4e3
Brandon Lawanto <banansbby@gmail.com>
2026-03-29 23:32:20 +0700

    fix: double booking list in navbar

--- final-project/src/components/navbar.tsx
+++ final-project/src/components/navbar.tsx
@@ -62,13 +62,6 @@ export default function Navbar() {
           >
             Konsultasi
           </Link>
-          <Link
-            key="bookinglist"
-            href="/bookinglist"
-            className="text-gray-500 hover:text-blue-700 transition-colors"
-          >
-            List booking
-          </Link>
           {session && (
             <Link
               key="bookinglist"
@@ -140,13 +133,6 @@ export default function Navbar() {
             className="block py-3 text-sm font-medium text-gray-600 border-b border-gray-50"
           >
             Konsultasi
-          </Link>
-          <Link
-            key="bookinglist"
-            href="/bookinglist"
-            className="block py-3 text-sm font-medium text-gray-600 border-b border-gray-50"
-          >
-            List booking
           </Link>
           {session && (
             <Link
